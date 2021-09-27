ca37354a6bb660b5cc360b66c9bb7820c822df60
daoraCode <severin.mboukou@gmail.com>
2021-09-27 12:16:54 +0200

    first challenge completed

---
+++ jour01.js
@@ -0,0 +1,35 @@
+// 1.
+console.log('Hello World !');
+
+// 2.
+var test = 'My name is Séverin';
+console.log(test);
+
+// 3.
+var fitstName = 'Séverin';
+console.log('Nice to meet you, ' + fitstName);
+
+// 4.
+var testLenght = "I'm very long";
+console.log(testLenght.length);
+
+// 5.
+var food = 'croissant is meh';
+console.log(food.replace('meh', 'so good'));
+
+// 6.
+var basic = 'This is Cool';
+var basicUp = 'THIS IS COOL';
+var basicDOwn = 'this is cool';
+console.log(basic);
+console.log(basicUp);
+console.log(basicDOwn);
+
+// 7.
+var word = 'banana';
+var letters = word.split();
+console.log(letters);
+
+var age = 27;
+var template = `I'm ${age} years old`;
+console.log(template);
