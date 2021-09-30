9a3d2dfe9d0171faf749c84df750e1d207633884
daoraCode <severin.mboukou@gmail.com>
2021-09-30 15:43:54 +0200

    Démarrage du fichier jour4

--- jour3-part2.js
+++ jour3-part2.js
@@ -4,7 +4,7 @@
 // var startRange = 11;
 // var nb = 0;
 // var sum = 0; // variable globale
-// // boucle for pour afficher les nombres pairs (0, 2, 4, 6, 8) de 11 à 47
+// boucle for pour afficher les nombres pairs (0, 2, 4, 6, 8) de 11 à 47
 // for (var i = startRange; i < limitRange; i++) {
 //   var nb = i; // variable locale
 //   if (nb % 2 === 0) {
@@ -19,15 +19,14 @@
 // var startRange = 109;
 // var newNb = 0;
 // var newSum = 0; // variable globale
-// // boucle for pour afficher les nombres pairs (0, 2, 4, 6, 8) de 11 à 47
+// boucle for pour afficher les nombres pairs (0, 2, 4, 6, 8) de 11 à 47
 // for (var i = startRange; i < limitRange; i++) {
 //   var newNb = i; // variable locale
 //   if (newNb % 2 !== 0) {
-//     // console.log(i + ' : IMPAIR');
 //     newSum = newSum + newNb;
 //   }
 // }
-// // console.log(newSum + ' : Somme de nombre impairs');
+// console.log(newSum + ' : Somme de nombre impairs');
 
 // ## 3 - Puissance et racine carrée
 // 3. 1 - A l'aide d'une boucle for, boucler a travers le tableau numbers. Remplir le tableau squares avec la puissance 2
@@ -68,23 +67,16 @@
 // - A l'aide d'une boucle `for`, afficher les nombres de 0 à 10 (avec `i`)
 // - Dans cette boucle `for`, créer une autre boucle `for` (utiliser `j` a la place de `i` en tant qu'index) allant elle aussi de 0 à 10
 // - Afficher les tables de multiplications de chaque chiffres de 0 à 10, en sautant une ligne entre chaque tables.
-var mainArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var arrayToMultiply = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var mainOperand = 'x';
-for (var i = 0; i < mainArray.length; i++) {
-  console.log(
-    `${mainArray[j]} ${mainOperand} ${arrayToMultiply[i]} = ${
-      mainArray[j] * arrayToMultiply[i]
-    }` + '\n'
-  );
-  for (var j = 0; j < mainArray.length; j++) {
-    console.log(
-      `${mainArray[i]} ${mainOperand} ${arrayToMultiply[j]} = ${
-        mainArray[i] * arrayToMultiply[j]
-      }`
-    );
-  }
-}
+// var mainOperand = 'x
+// for (var i = 0; i < mainArray.length; i++) {
+//   for (var j = 0; j < mainArray.length; j++) {
+//     console.log(
+//       `${mainArray[i]} ${mainOperand} ${arrayToMultiply[j]} = ${
+//         mainArray[i] * arrayToMultiply[j]
+//       }`
+//     );
+//   }
+// }
 
 
 // ## 6 - Factorielle v1
@@ -92,4 +84,28 @@ for (var i = 0; i < mainArray.length; i++) {
 // des nombres entiers positifs inférieur ou égal à lui meme. 
 // On le note généralement avec un point d'exclamation après le nombre.
 // A l'aide d'une boucle `for`, calculer la factorielle de 7.
+// var factorial = 7;
+// for(var i = 1; i < 7; i++) {
+//   factorial = factorial * i;
+// }
+// console.log(factorial);
+// var result = 0;
+
+
+// ## 7 - Factorielle v2
+// En utilisant deux boucles `for`(l'une imbriquée dans l'autre), afficher les factorielles de 0 a 8 inclus.
+// - Indice: factorielle de 0 et factorielle de 1 donnent toutes les deux le meme résultat: 1
+var factorial = 7;
+for(var i = 1; i < 7; i++) {
+  var factorial = 1;
+    for(var j = 1; j < j; j++) {
+    console.log(i);
+  }
+}
+console.log(factorial);
+
+
+
+
+
 
